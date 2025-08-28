a9039493f7e7b9fc4ef7d76b1b82e31c04cb5bdb
ines <ines@moonwit.ch>
2025-08-29 00:03:30 +0200

    unpass pkgs

--- modules/default.nix
+++ modules/default.nix
@@ -4,17 +4,6 @@
     {
       flake-file.inputs.home-manager.url = lib.mkDefault "github:nix-community/home-manager";
 
-      perSystem =
-        { system, ... }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config = {
-              allowUnfreePredicate = _pkg: true;
-            };
-          };
-        };
-
       imports = [
         inputs.flake-file.flakeModules.dendritic
         ./configurations.nix
